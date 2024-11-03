ffb8dbb7dacd3d5a74d3a7d1b2aec7fc46d13623
alikalatearabi <ali.kalate89@hotmail.com>
2024-11-03 22:49:18 +0330

    make widgets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:main_app/screens/personal_info/personal_info_confirmation.dart';
-import 'screens/splash_screen.dart';
+import 'widgets/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
