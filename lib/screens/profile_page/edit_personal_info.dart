3f889ca46f9ebf7c4e6636f5deeb2489a75db9c2
Samira Ahmadnezhad <samiraahmadnezhad2004@gmail.com>
2025-09-24 21:45:11 +0330

    add change profile widget

--- lib/screens/profile_page/edit_personal_info.dart
+++ lib/screens/profile_page/edit_personal_info.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditPersonalInfo extends StatefulWidget {
   const EditPersonalInfo({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class _EditPersonalInfoState extends State<EditPersonalInfo> {
   late TextEditingController nameController;
   late TextEditingController phoneController;
   late TextEditingController emailController;
+  File? _imageFile;
 
   @override
   void initState() {
@@ -30,10 +33,58 @@ class _EditPersonalInfoState extends State<EditPersonalInfo> {
     super.dispose();
   }
 
+  Future<void> _pickImage(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _showImageSourceActionSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.photo_library),
+                  title: const Text('انتخاب از گالری'),
+                  onTap: () {
+                    _pickImage(ImageSource.gallery);
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('گرفتن عکس با دوربین'),
+                  onTap: () {
+                    _pickImage(ImageSource.camera);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(bottom: 25, left: 24, right: 24, top: 10),
+      padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom + 25,
+          left: 24,
+          right: 24,
+          top: 10),
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.vertical(
@@ -42,136 +93,126 @@ class _EditPersonalInfoState extends State<EditPersonalInfo> {
       ),
       child: Directionality(
         textDirection: TextDirection.rtl,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              height: 5,
-              width: 50,
-              margin: const EdgeInsets.only(bottom: 20),
-              decoration: const BoxDecoration(
-                color: Color(0xFFE3E6EA),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 5,
+                width: 50,
+                margin: const EdgeInsets.only(bottom: 20),
+                decoration: const BoxDecoration(
+                  color: Color(0xFFE3E6EA),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
               ),
-            ),
-
-             Align(
-               alignment: Alignment.centerRight,
-               child: Text(
-                'اطلاعات حساب',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                           ),
-             ),
-
-            const SizedBox(height: 20),
-
-            Center(
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage('assets/profile_avatar.png'),
-                        ),
-                        border: Border.fromBorderSide(
-                          BorderSide(color: Color(0xFF018A08), width: 3),
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'اطلاعات حساب',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: _imageFile != null
+                                ? FileImage(_imageFile!)
+                                : const AssetImage('assets/profile_avatar.png')
+                            as ImageProvider,
+                          ),
+                          border: const Border.fromBorderSide(
+                            BorderSide(color: Color(0xFF018A08), width: 3),
+                          ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFF018A08),
-                        ),
-                        child: InkWell(
-                          onTap: (){
-                            showModalBottomSheet(
-                              context: context,
-                              isScrollControlled: true,
-                              backgroundColor: Colors.transparent,
-                              builder: (context) => const EditPersonalInfo(),
-                            );
-
-                          },
-                          child: Center(
-                            child: SvgPicture.asset(
-                              'assets/icons/camera.svg',
-                              width: 18,
-                              height: 18,
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF018A08),
+                          ),
+                          child: InkWell(
+                            onTap: _showImageSourceActionSheet,
+                            child: Center(
+                              child: SvgPicture.asset(
+                                'assets/icons/camera.svg',
+                                width: 18,
+                                height: 18,
+                              ),
                             ),
                           ),
                         ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              buildInfoField(
+                label: 'نام و نام‌خانوادگی',
+                controller: nameController,
+              ),
+              const SizedBox(height: 16),
+              buildInfoField(
+                label: 'شماره موبایل',
+                controller: phoneController,
+                enabled: false,
+              ),
+              const SizedBox(height: 16),
+              buildInfoField(
+                label: 'ایمیل',
+                controller: emailController,
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF018A08),
+                  elevation: 0,
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  minimumSize: Size(
+                    MediaQuery.of(context).size.width * 0.88,
+                    MediaQuery.of(context).size.height * 0.06,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(width: 4),
+                    Text(
+                      "تایید و ذخیره",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-
-            const SizedBox(height: 40),
-
-            buildInfoField(
-              label: 'نام و نام‌خانوادگی',
-              controller: nameController,
-            ),
-            const SizedBox(height: 16),
-            buildInfoField(
-              label: 'شماره موبایل',
-              controller: phoneController,
-              enabled: false,
-            ),
-            const SizedBox(height: 16),
-            buildInfoField(
-              label: 'ایمیل',
-              controller: emailController,
-            ),
-
-            const SizedBox(height: 24),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF018A08),
-                elevation: 0,
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                minimumSize: Size(
-                  MediaQuery.of(context).size.width * 0.88,
-                  MediaQuery.of(context).size.height * 0.06,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 4),
-                  Text(
-                    "تایید و ذخیره",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -200,14 +241,23 @@ class _EditPersonalInfoState extends State<EditPersonalInfo> {
           decoration: InputDecoration(
             filled: true,
             fillColor: enabled ? Colors.white : Colors.grey.shade100,
-            contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+            contentPadding:
+            const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(12),
               borderSide: BorderSide(color: Colors.grey.shade300),
             ),
-            disabledBorder: OutlineInputBorder(
+            enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(12),
               borderSide: BorderSide(color: Colors.grey.shade300),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.grey.shade500),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.grey.shade200),
             ),
           ),
           style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
